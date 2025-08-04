a45ac3c408efd8d050f334bfb7e5e8a295a13ba1
Sebastian Acevedo <sacevedog2@eafit.edu.co>
2025-08-04 14:55:49 -0500

    actividad 2 django modelos, falta agregar los comentarios desde la db

--- pages/views.py
+++ pages/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponseRedirect
 from django.urls import reverse
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
 from django.views import View
 from django import forms
+from .models import Product
 # Create your views here.
 
 class HomePageView(TemplateView):
@@ -25,20 +26,14 @@ class AboutPageView(TemplateView):
 class ContactPageView(TemplateView):
     template_name = "pages/contact.html"
 
-class Product:
- products = [
-    {"id":"1", "name":"TV", "description":"Best TV", "price": 1299.99},
-    {"id":"2", "name":"iPhone", "description":"Best iPhone", "price": 1199.99},
-    {"id":"3", "name":"Chromecast", "description":"Best Chromecast", "price": 73.59},
-    {"id":"4", "name":"Glasses", "description":"Best Glasses", "price": 50.25},
-    ]
+
 class ProductIndexView(View):
     template_name = 'pages/products/index.html'
     def get(self, request):
         viewData = {}
         viewData["title"] = "Products - Online Store"
         viewData["subtitle"] = "List of products"
-        viewData["products"] = Product.products
+        viewData["products"] = Product.objects.all()
         return render(request, self.template_name, viewData)
 
 class ProductShowView(View):
@@ -47,19 +42,21 @@ class ProductShowView(View):
     def get(self, request, id):
         viewData = {}
         try:
-            product = Product.products[int(id)-1]
-        except (IndexError, ValueError):
+            product_id = int(id)
+            product = get_object_or_404(Product, pk=product_id)
+        except (ValueError, Product.DoesNotExist):
             return HttpResponseRedirect(reverse('home'))
-        
-        viewData["title"] = product["name"] + " - Online Store"
-        viewData["subtitle"] = product["name"] + " - Product information"
+
+        viewData["title"] = product.name + " - Online Store"
+        viewData["subtitle"] = product.name + " - Product information"
         viewData["product"] = product
         return render(request, self.template_name, viewData)
 
-class ProductForm(forms.Form):
-    name = forms.CharField(label="Product Name", required=True, max_length=100)
-    description = forms.CharField(label="Description", required=True, widget=forms.Textarea)
-    price = forms.FloatField(label="Price", required=True)
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = ['name', 'description', 'price']
+    description = forms.CharField(required=False)
 
     def clean_price(self):
         price = self.cleaned_data.get('price')
@@ -69,22 +66,33 @@ class ProductForm(forms.Form):
 
     
     
-class ProductCreateView(View):
+class ProductCreateView(View): 
     template_name = 'pages/products/create.html'
 
     def get(self, request):
         form = ProductForm()
-        return render(request, self.template_name, {"form": form, "title": "Add Product - Online Store"})
+        return render(request, self.template_name, {
+            "form": form,
+            "title": "Add Product - Online Store"
+        })
 
     def post(self, request):
         form = ProductForm(request.POST)
         if form.is_valid():
-            new_product = {
-                "id": str(len(Product.products) + 1),
-                "name": form.cleaned_data["name"],
-                "description": form.cleaned_data["description"],
-                "price": form.cleaned_data["price"],
-            }
-            Product.products.append(new_product)
-            return redirect('products.index')  # Asegúrate de tener esta ruta
-        return render(request, self.template_name, {"form": form, "title": "Add Product - Online Store"})
+            form.save()  # ✅ Aquí usas form.save() gracias a ModelForm
+            return redirect('products.index')
+        return render(request, self.template_name, {
+            "form": form,
+            "title": "Add Product - Online Store"
+        })
+
+class ProductListView(ListView): 
+    model = Product 
+    template_name = 'product_list.html' 
+    context_object_name = 'products'  # This will allow you to loop through 'products' in your template 
+
+    def get_context_data(self, **kwargs): 
+        context = super().get_context_data(**kwargs) 
+        context['title'] = 'Products - Online Store' 
+        context['subtitle'] = 'List of products' 
+        return context  
